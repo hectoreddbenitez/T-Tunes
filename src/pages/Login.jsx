b13e05f770193910dc48b119f0b7b050b1fa78ab
hector <hectoreddbenitez@gmail.com>
2021-09-20 18:15:25 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      button: true,
+      value: '',
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  // Obrigado Isaac Matheus!!
+
+  handleBtnEnabel = ({ target: { value } }) => {
+    const caracterMin = 3;
+    this.setState({
+      button: value.length < caracterMin,
+      value,
+    });
+  };
+
+  handelOnClick = async () => {
+    const { value } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: value });
+    this.setState({ loading: false, redirect: true });
+  };
+
   render() {
+    const { button, value, loading, redirect } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form action="">
+          <input
+            // name="value"
+            value={ value }
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.handleBtnEnabel }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ button }
+            onClick={ this.handelOnClick }
+          >
+            Entrar
+          </button>
+          {loading ? <Loading /> : null }
+          {redirect ? <Redirect to="/search" /> : null }
+        </form>
+      </div>
     );
   }
 }
