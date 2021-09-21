38d7e7c2edde27bfc06b32f3dcb631161f8aafdf
hector <hectoreddbenitez@gmail.com>
2021-09-21 13:46:18 -0300

    Requisito 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      dadosUsuario: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setEstate();
+  }
+
+  setEstate= () => {
+    this.setState({ loading: true }, async () => {
+      const dadosUsuario = await getUser();
+      this.setState({ dadosUsuario, loading: false });
+    });
+  }
+
   render() {
+    const { dadosUsuario: { name }, loading } = this.state;
     return (
       <header data-testid="header-component">
-        oi
+        <div data-testid="header-user-name">
+          { name }
+          {loading ? <Loading /> : null }
+        </div>
       </header>
     );
   }
